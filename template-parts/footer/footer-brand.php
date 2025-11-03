dfa5c3b484ccb8b6fc90cf7a297f6d8bd69c6959
Creastv <creastv@gail.com>
2025-11-03 08:46:04 +0100

    Version: 1.0.3

--- template-parts/footer/footer-brand.php
+++ template-parts/footer/footer-brand.php
@@ -24,11 +24,11 @@ if (!defined('ABSPATH')) {
                 $footer_logo_url = wp_get_attachment_image_url($footer_logo_id, 'full');
                 if ($footer_logo_url) {
             ?>
-                    <a href="<?php echo esc_url(avsec_get_home_url()); ?>" rel="home" class="footer-logo-link">
-                        <img src="<?php echo esc_url($footer_logo_url); ?>" alt="<?php echo esc_attr(get_bloginfo('name')); ?>"
-                            style="max-width: <?php echo absint($footer_logo_width); ?>px; height: auto;">
-                    </a>
-                    <?php
+            <a href="<?php echo esc_url(avsec_get_home_url()); ?>" rel="home" class="footer-logo-link">
+                <img src="<?php echo esc_url($footer_logo_url); ?>" alt="<?php echo esc_attr(get_bloginfo('name')); ?>"
+                    style="max-width: <?php echo absint($footer_logo_width); ?>px; height: auto;">
+            </a>
+            <?php
                 }
             } else {
                 // Try to use header logo if footer logo is not set
@@ -37,20 +37,20 @@ if (!defined('ABSPATH')) {
                     $header_logo_url = wp_get_attachment_image_url($header_logo_id, 'full');
                     if ($header_logo_url) {
                     ?>
-                        <a href="<?php echo esc_url(avsec_get_home_url()); ?>" rel="home" class="footer-logo-link">
-                            <img src="<?php echo esc_url($header_logo_url); ?>" alt="<?php echo esc_attr(get_bloginfo('name')); ?>"
-                                style="max-width: <?php echo absint($footer_logo_width); ?>px; height: auto;">
-                        </a>
-                    <?php
+            <a href="<?php echo esc_url(avsec_get_home_url()); ?>" rel="home" class="footer-logo-link">
+                <img src="<?php echo esc_url($header_logo_url); ?>" alt="<?php echo esc_attr(get_bloginfo('name')); ?>"
+                    style="max-width: <?php echo absint($footer_logo_width); ?>px; height: auto;">
+            </a>
+            <?php
                     }
                 } else {
                     // No logo at all - show site title
                     ?>
-                    <h3 class="footer-site-title">
-                        <a href="<?php echo esc_url(avsec_get_home_url()); ?>" rel="home">
-                            <?php bloginfo('name'); ?>
-                        </a>
-                    </h3>
+            <h3 class="footer-site-title">
+                <a href="<?php echo esc_url(avsec_get_home_url()); ?>" rel="home">
+                    <?php bloginfo('name'); ?>
+                </a>
+            </h3>
             <?php
                 }
             }
@@ -78,44 +78,41 @@ if (!defined('ABSPATH')) {
             $footer_address = get_theme_mod('footer_address', '');
             ?>
             <?php if ($footer_address) : ?>
-                <div class="contact-item">
-                    <span class="contact-icon">
-                        <svg width="35" height="33" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path
-                                d="M12.9998 23.8333C12.7471 23.8333 12.5304 23.7611 12.3498 23.6166C12.1693 23.4722 12.0339 23.2826 11.9436 23.0479C11.6005 22.0368 11.1672 21.0888 10.6436 20.2041C10.138 19.3194 9.42484 18.2812 8.504 17.0895C7.58317 15.8979 6.83387 14.7604 6.25609 13.677C5.69636 12.5937 5.4165 11.2847 5.4165 9.74996C5.4165 7.63746 6.14775 5.84996 7.61025 4.38746C9.09081 2.9069 10.8873 2.16663 12.9998 2.16663C15.1123 2.16663 16.8998 2.9069 18.3623 4.38746C19.8429 5.84996 20.5832 7.63746 20.5832 9.74996C20.5832 11.393 20.2672 12.7652 19.6353 13.8666C19.0214 14.95 18.3082 16.0243 17.4957 17.0895C16.5207 18.3895 15.7804 19.4729 15.2748 20.3395C14.7873 21.1882 14.3811 22.0909 14.0561 23.0479C13.9658 23.3007 13.8214 23.4993 13.6228 23.6437C13.4422 23.7701 13.2346 23.8333 12.9998 23.8333ZM12.9998 12.4583C13.7582 12.4583 14.3991 12.1965 14.9228 11.6729C15.4464 11.1493 15.7082 10.5083 15.7082 9.74996C15.7082 8.99162 15.4464 8.35065 14.9228 7.82704C14.3991 7.30343 13.7582 7.04163 12.9998 7.04163C12.2415 7.04163 11.6005 7.30343 11.0769 7.82704C10.5533 8.35065 10.2915 8.99162 10.2915 9.74996C10.2915 10.5083 10.5533 11.1493 11.0769 11.6729C11.6005 12.1965 12.2415 12.4583 12.9998 12.4583Z"
-                                stroke="#1CAFED" />
-                        </svg>
+            <div class="contact-item">
+                <span class="contact-icon">
+                    <svg width="20" height="26" viewBox="0 0 18 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M9 22.8332C8.74723 22.8332 8.53056 22.761 8.35001 22.6165C8.16945 22.4721 8.03403 22.2825 7.94376 22.0478C7.6007 21.0366 7.16737 20.0887 6.64375 19.204C6.1382 18.3193 5.425 17.2811 4.50417 16.0894C3.58334 14.8978 2.83403 13.7603 2.25625 12.6769C1.69653 11.5936 1.41667 10.2846 1.41667 8.74984C1.41667 6.63734 2.14792 4.84984 3.61042 3.38734C5.09098 1.90678 6.8875 1.1665 9 1.1665C11.1125 1.1665 12.9 1.90678 14.3625 3.38734C15.8431 4.84984 16.5833 6.63734 16.5833 8.74984C16.5833 10.3929 16.2674 11.7651 15.6354 12.8665C15.0215 13.9498 14.3083 15.0241 13.4958 16.0894C12.5208 17.3894 11.7806 18.4728 11.275 19.3394C10.7875 20.188 10.3813 21.0908 10.0563 22.0478C9.96598 22.3005 9.82153 22.4991 9.62292 22.6436C9.44237 22.77 9.23473 22.8332 9 22.8332ZM9 11.4582C9.75834 11.4582 10.3993 11.1964 10.9229 10.6728C11.4465 10.1491 11.7083 9.50817 11.7083 8.74984C11.7083 7.9915 11.4465 7.35053 10.9229 6.82692C10.3993 6.30331 9.75834 6.0415 9 6.0415C8.24167 6.0415 7.6007 6.30331 7.07709 6.82692C6.55348 7.35053 6.29167 7.9915 6.29167 8.74984C6.29167 9.50817 6.55348 10.1491 7.07709 10.6728C7.6007 11.1964 8.24167 11.4582 9 11.4582Z"
+                            stroke="#1cafed"></path>
+                    </svg>
 
-                    </span>
-                    <span class="contact-text"><?php echo esc_html($footer_address); ?></span>
-                </div>
+                </span>
+                <span class="contact-text"><?php echo esc_html($footer_address); ?></span>
+            </div>
             <?php endif; ?>
 
             <?php if ($footer_email) : ?>
-                <div class="contact-item">
-                    <span class="contact-icon">
-                        <svg width="25" height="25" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path
-                                d="M2.16699 13.0002C2.16699 8.91466 2.16699 6.8719 3.4362 5.6027C4.7054 4.3335 6.74815 4.3335 10.8337 4.3335H15.167C19.2525 4.3335 21.2952 4.3335 22.5645 5.6027C23.8337 6.8719 23.8337 8.91466 23.8337 13.0002C23.8337 17.0857 23.8337 19.1284 22.5645 20.3976C21.2952 21.6668 19.2525 21.6668 15.167 21.6668H10.8337C6.74815 21.6668 4.7054 21.6668 3.4362 20.3976C2.16699 19.1284 2.16699 17.0857 2.16699 13.0002Z"
-                                stroke="#1CAFED" stroke-width="2" />
-                            <path
-                                d="M6.5 8.6665L8.8388 10.6155C10.8285 12.2736 11.8233 13.1026 13 13.1026C14.1767 13.1026 15.1715 12.2736 17.1612 10.6155L19.5 8.6665"
-                                stroke="#1CAFED" stroke-width="2" stroke-linecap="round" />
-                        </svg>
-                    </span>
-                    <a href="mailto:<?php echo esc_attr($footer_email); ?>" class="contact-link">
-                        <?php echo esc_html($footer_email); ?>
-                    </a>
-                </div>
+            <div class="contact-item">
+                <span class="contact-icon">
+                    <svg width="23" height="16" viewBox="0 0 23 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M21.9167 3.24984C21.9167 2.104 20.9792 1.1665 19.8333 1.1665H3.16668C2.02084 1.1665 1.08334 2.104 1.08334 3.24984M21.9167 3.24984V15.7498C21.9167 16.8957 20.9792 17.8332 19.8333 17.8332H3.16668C2.02084 17.8332 1.08334 16.8957 1.08334 15.7498V3.24984M21.9167 3.24984L11.5 10.5415L1.08334 3.24984"
+                            stroke="#1cafed" stroke-linecap="round" stroke-linejoin="round"></path>
+                    </svg>
+                </span>
+                <a href="mailto:<?php echo esc_attr($footer_email); ?>" class="contact-link">
+                    <?php echo esc_html($footer_email); ?>
+                </a>
+            </div>
             <?php endif; ?>
 
             <?php if ($footer_phone) : ?>
-                <div class="contact-item">
-                    <span class="contact-icon">📞</span>
-                    <a href="tel:<?php echo esc_attr($footer_phone); ?>" class="contact-link">
-                        <?php echo esc_html($footer_phone); ?>
-                    </a>
-                </div>
+            <div class="contact-item">
+                <span class="contact-icon">📞</span>
+                <a href="tel:<?php echo esc_attr($footer_phone); ?>" class="contact-link">
+                    <?php echo esc_html($footer_phone); ?>
+                </a>
+            </div>
             <?php endif; ?>
         </div>
         <?php
@@ -123,11 +120,11 @@ if (!defined('ABSPATH')) {
         $contact_button_url = get_theme_mod('contact_button_url', '#');
         if ($contact_button_text && $contact_button_url) :
         ?>
-            <div class="footer-contact-button">
-                <a href="<?php echo esc_url($contact_button_url); ?>" class="btn btn-outline-primary">
-                    <?php echo esc_html($contact_button_text); ?>
-                </a>
-            </div>
+        <div class="footer-contact-button">
+            <a href="<?php echo esc_url($contact_button_url); ?>" class="btn btn-outline-primary">
+                <?php echo esc_html($contact_button_text); ?>
+            </a>
+        </div>
         <?php endif; ?>
 
     </div>
