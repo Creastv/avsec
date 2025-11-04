b23d17a822657d9866f8792c77c4ae7a4e62b870
Creastv <creastv@gail.com>
2025-11-04 17:33:43 +0100

    Version: 1.0.3

--- page-form.php
+++ page-form.php
@@ -12,27 +12,27 @@ get_header(); ?>
 <main class="site-main">
     <div class="container">
         <?php while (have_posts()) : the_post(); ?>
-            <article id="post-<?php the_ID(); ?>" class="page page-form">
-                <div class="page-form__grid">
-                    <div class="page-form__content">
-                        <header class="page-form__header">
-                            <?php the_title('<h1 class="page-form__title">', '</h1>'); ?>
-                            <?php if (function_exists('yoast_breadcrumb')) : ?>
-                                <nav class="breadcrumbs" aria-label="Breadcrumbs">
-                                    <?php yoast_breadcrumb('', ''); ?>
-                                </nav>
-                            <?php endif; ?>
+        <article id="post-<?php the_ID(); ?>" class="page page-form">
+            <div class="page-form__grid">
+                <div class="page-form__content">
+                    <header class="page-form__header <?php echo has_post_thumbnail() ? 'has-thumbnail' : ''; ?>">
+                        <?php the_title('<h1 class="page-form__title">', '</h1>'); ?>
+                        <?php if (function_exists('yoast_breadcrumb')) : ?>
+                        <nav class="breadcrumbs" aria-label="Breadcrumbs">
+                            <?php yoast_breadcrumb('', ''); ?>
+                        </nav>
+                        <?php endif; ?>
 
-                            <?php if (has_post_thumbnail()) : ?>
-                                <div class="post-thumbnail">
-                                    <?php the_post_thumbnail('large'); ?>
-                                </div>
-                            <?php endif; ?>
-                        </header>
+                        <?php if (has_post_thumbnail()) : ?>
+                        <div class="post-thumbnail">
+                            <?php the_post_thumbnail('large'); ?>
+                        </div>
+                        <?php endif; ?>
+                    </header>
 
 
-                        <div class="post-content">
-                            <?php
+                    <div class="post-content">
+                        <?php
                             the_content();
 
                             wp_link_pages(array(
@@ -40,20 +40,20 @@ get_header(); ?>
                                 'after'  => '</div>',
                             ));
                             ?>
-                        </div>
                     </div>
-
-                    <aside class="page-form__sidebar" aria-label="Formularz kontaktowy">
-                        <div class="page-form__sticky">
-                            <?php echo do_shortcode('[contact-form-7 id="7507adf" title="Formularz na podstronach"]'); ?>
-                        </div>
-                    </aside>
                 </div>
-            </article>
+
+                <aside class="page-form__sidebar" aria-label="Formularz kontaktowy">
+                    <div class="page-form__sticky">
+                        <?php echo do_shortcode('[contact-form-7 id="7507adf" title="Formularz na podstronach"]'); ?>
+                    </div>
+                </aside>
+            </div>
+        </article>
         <?php endwhile; ?>
     </div>
     <?php if (is_singular('szkolenia')): ?>
-        <?php get_template_part('template-parts/parts/recomended-training'); ?>
+    <?php get_template_part('template-parts/parts/recomended-training'); ?>
 
     <?php endif; ?>
 </main>
