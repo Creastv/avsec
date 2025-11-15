0f339ab3cfdf651dfc093f212d12aca206284509
Creastv <creastv@gail.com>
2025-11-15 09:53:53 +0100

    nav

--- template-parts/content/content-post.php
+++ template-parts/content/content-post.php
@@ -3,10 +3,10 @@
         <div class="post-thumbnail">
             <a href="<?php the_permalink(); ?>">
                 <?php if (has_post_thumbnail()) : ?>
-                    <?php the_post_thumbnail('medium'); ?>
+                <?php the_post_thumbnail('medium'); ?>
                 <?php else : ?>
-                    <img src="<?php echo esc_url(get_template_directory_uri() . '/assets/img/thumbnail.png'); ?>"
-                        alt="<?php echo esc_attr(get_the_title()); ?>" class="placeholder-thumb" />
+                <img src="<?php echo esc_url(get_template_directory_uri() . '/assets/img/thumbnail.png'); ?>"
+                    alt="<?php echo esc_attr(get_the_title()); ?>" class="placeholder-thumb" />
                 <?php endif; ?>
             </a>
         </div>
@@ -17,15 +17,15 @@
                 <?php the_title(); ?>
             </a>
         </h2>
-        <?php echo custom_excerpt(30); ?>
+        <p><?php echo custom_excerpt(30); ?></p>
     </div>
 
     <?php if (is_user_logged_in() && current_user_can('administrator')) : ?>
-        <footer class="post-footer">
-            <a href="<?php echo get_edit_post_link(); ?>" class="edit-link" style="margin-left:10px;">
-                Edytuj
-            </a>
-        </footer>
+    <footer class="post-footer">
+        <a href="<?php echo get_edit_post_link(); ?>" class="edit-link" style="margin-left:10px;">
+            Edytuj
+        </a>
+    </footer>
     <?php endif; ?>
 
 </article>
