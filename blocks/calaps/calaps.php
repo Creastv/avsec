62765f8f0ee1a3f96df907bb49a08ea7be0978d2
Creastv <creastv@gail.com>
2025-11-28 17:41:51 +0100

    hero

--- blocks/calaps/calaps.php
+++ blocks/calaps/calaps.php
@@ -5,17 +5,17 @@ $block_id = 'calaps-' . $block['id'];
 
 if ($title || $calaps) :
 ?>
-    <div class="calaps-block" id="<?php echo esc_attr($block_id); ?>">
-        <div class="calaps-item">
-            <button class="calaps-header" aria-expanded="false" aria-controls="<?php echo esc_attr($block_id); ?>-content">
-                <h3 class="calaps-title"><?php echo esc_html($title); ?></h3>
-                <span class="calaps-icon" aria-hidden="true"></span>
-            </button>
-            <div class="calaps-content" id="<?php echo esc_attr($block_id); ?>-content" aria-hidden="true">
-                <div class="calaps-inner">
-                    <?php echo $calaps; ?>
-                </div>
+<div class="calaps-block" id="<?php echo esc_attr($block_id); ?>">
+    <div class="calaps-item">
+        <button class="calaps-header" aria-expanded="false" aria-controls="<?php echo esc_attr($block_id); ?>-content">
+            <h2 class="calaps-title"><?php echo esc_html($title); ?></h2>
+            <span class="calaps-icon" aria-hidden="true"></span>
+        </button>
+        <div class="calaps-content" id="<?php echo esc_attr($block_id); ?>-content" aria-hidden="true">
+            <div class="calaps-inner">
+                <?php echo $calaps; ?>
             </div>
         </div>
     </div>
+</div>
 <?php endif; ?>
