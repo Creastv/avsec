897de490bafea96f6ee98b119b2450d5832beb20
Creastv <creastv@gail.com>
2025-11-28 09:23:21 +0100

    hero

--- blocks/calaps/calaps.php
+++ blocks/calaps/calaps.php
@@ -8,7 +8,7 @@ if ($title || $calaps) :
     <div class="calaps-block" id="<?php echo esc_attr($block_id); ?>">
         <div class="calaps-item">
             <button class="calaps-header" aria-expanded="false" aria-controls="<?php echo esc_attr($block_id); ?>-content">
-                <span class="calaps-title"><?php echo esc_html($title); ?></span>
+                <h3 class="calaps-title"><?php echo esc_html($title); ?></h3>
                 <span class="calaps-icon" aria-hidden="true"></span>
             </button>
             <div class="calaps-content" id="<?php echo esc_attr($block_id); ?>-content" aria-hidden="true">
